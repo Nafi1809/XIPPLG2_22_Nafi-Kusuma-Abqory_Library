724b4fa0fa27ed7b50ae9f64675f1f40cea2c585
unknown <nafisantuyy@gmail.com>
2025-02-05 08:17:04 +0700

    Add Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $table = 'categories';
+    protected $fillable = ['name'];
+
+    public function books()
+    {
+        return $this->hasMany(Book::class, 'category_id');
+    }
 }
